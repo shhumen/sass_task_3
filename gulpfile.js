14c487ec8970db902d6539cef443baa95b3d57ff
shhumen <mehtiyevashumen@gmail.com>
2023-12-23 22:28:07 +0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+// node_modules klasörü içerisinden gulp kütüphanesini çağırıyoruz
+const { src, dest, watch, series } = require('gulp')
+
+// gulp-sass kütüphanesini çağırıyoruz
+const sass = require('gulp-sass')(require('sass'))
+
+// hangi dosyanın, hangi dosya ile değiştirileceğini belirtiyoruz
+function buildStyles() {
+  return src('sass/**/*.scss').pipe(sass()).pipe(dest('css'))
+}
+
+//  /* /**/ means folders in sass */   */
+
+// index.scss dosyasında değişiklik olduğunda buildStyles fonksiyonunu çalıştır
+
+function watchTask() {
+  return watch(['sass/**/*.scss'], buildStyles)
+}
+
+// default olarak çalıştırılacak fonksiyonu belirtiyoruz
+exports.default = series(buildStyles, watchTask)
